674634bf9f66c5cf561d7e42ee95e1e61280a606
saumya sakshi <saumyasakshi41@gmail.com>
2020-12-21 10:21:49 +0530

    first commit

---
+++ app/src/main/java/com/example/newsapplication/activity_splash_screen.java
@@ -0,0 +1,32 @@
+package com.example.newsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import static java.lang.Thread.sleep;
+
+
+
+public class activity_splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1500);
+                    Intent intent = new Intent(activity_splash_screen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        });thread.start();
+    }
+}
